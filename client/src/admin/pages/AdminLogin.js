43bef965fec611934d5954ba78014486a720912b
Carlo Amaquin <amaquincj01@gmail.com>
2025-02-27 06:44:08 +0800

    supabase database

--- client/src/admin/pages/AdminLogin.js
+++ client/src/admin/pages/AdminLogin.js
@@ -6,7 +6,7 @@ const AdminLogin = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  const API_BASE_URL = "https://tdms-panglao-client.onrender.com";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: "postgresql://tdms_admin:Cg1jdL6CUiBuxuBgzZ8Nttxg5Tiv0cL5@dpg-cuvauct6l47c738pm0hg-a.singapore-postgres.render.com/tdms_db",
+  connectionString: "postgresql://postgres.xfxhhyfnqvcvarmloqrm:@maquinCJ00@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
   ssl: { rejectUnauthorized: false }, // Required for Render's PostgreSQL
 });
 
