b2dc403b8fd60b0c03b56b9f0b9937b72e7be591
Carlo Amaquin <amaquincj01@gmail.com>
2025-10-27 22:51:08 +0800

    fix(authControler.js):update the login link in your approval email to use your new domain or server address instead of the old Render link

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -206,11 +206,12 @@ exports.signup = async (req, res) => {
     // Send approval email if auto-approval is enabled
     if (is_approved) {
       const subject = "Your TDMS Account Has Been Approved";
+      const loginUrl = `${process.env.FRONTEND_URL}/login`;
       const message = `
         Dear Valued User,<br><br>
         We are pleased to inform you that your account registration for the Panglao Tourist Data Management System (TDMS) has been approved.<br><br>
         You may now log in and access the system using the following link:<br>
-        <a href="https://tdms-panglao-client.onrender.com">Login Link</a><br><br>
+        <a href="${loginUrl}">Login Link</a><br><br>
         If you have any questions or require assistance, please do not hesitate to contact our office.<br><br>
         Thank you for your interest in the TDMS.<br><br>
         Best regards,<br>
